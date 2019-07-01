377c8de2416b36275c0a2509a52b08ef80387640
Sam <sammintz1993@gmail.com>
2019-07-01 13:23:38 -0500

    Added menu bar

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "gatsby";
 
 const Menu = () => (
   <div style={{
@@ -7,15 +8,15 @@ const Menu = () => (
 
   }}>
 
-  <ul stlye={{
+  <ul style={{
     listStyle: "none",
     display: "flex",
     justifyContent: "space-evenly"
   }}>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/">About</Link></li>
+      <li><Link to="/">Recipes</Link></li>
+      <li><Link to="/">Blog</Link></li>
   </ul>
 
   </div>
