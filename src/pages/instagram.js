e32b4e16bb52839f3076cee4149574b257801107
Sam <sammintz1993@gmail.com>
2019-07-11 18:43:03 -0500

    Added arrows using react icons

--- src/pages/instagram.js
+++ src/pages/instagram.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import BackArrow from '../components/arrows/backArrow';
+import NextArrow from '../components/arrows/NextArrow';
+
 
 const API_KEY = process.env.GATSBY_INSTAGRAM_API_KEY
-
-// https://api.instagram.com/v1/users/self/media/recent/?access_token=11710304357.eff30cd.2e768e7e7e774d7185166f670650383b
 
 class Instagram extends Component {
   constructor(props){
@@ -16,51 +17,45 @@ class Instagram extends Component {
 
   componentWillMount(){
     console.log(" -- Component Will Mount -- ")
-    console.log(" -- API_KEY -- ")
-    console.log(API_KEY);
-    console.log(" -- API_KEY -- ")
+    // console.log(" -- API_KEY -- ")
+    // console.log(API_KEY);
+    // console.log(" -- API_KEY -- ")
     this.fetchPhotos();
   }
 
   fetchPhotos = () => {
     console.log("Api call for Insta photos")
-
     axios.get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${API_KEY}`)
       .then(response => {
-        console.log(response.data.data)
-      this.setState({
-        photos: response.data.data
-      })
+        this.setState({
+          photos: response.data.data
+        })
     })
   }
 
   render() {
-  //   {this.state.photos.map((photo, key) => {
-  //     return (
-  //       <div key={photo.id}>
-  //         <h1>Insta Feed...</h1>
-  //         {/* {console.log(this.state.photos, "this is that")} */}
-  //         <img src={photo.images.standard.resolution.url} alt={photo.caption}/>
-  //       </div>
-  //     );
-  //   })}
-
-  // }
+    { console.log(this.state.photos) }
 
 
-  const photoList = this.state.photos.map((photo, i) => {
-    return (
-      <div key={photo.id}>
-        <img src={photo.images.standard_resolution.url} />
-      </div>
-    );
-  })
+    const photoList = this.state.photos.map((photo, i) => {
+      return (
+        <div key={photo.id}>
+        
+          <img src={photo.images.standard_resolution.url} alt={photo.caption}/>
+          <div style={{width: '600px', margin: '24px auto', fontStyle: 'italic'}}>
+            {photo.caption !== null ? photo.caption.text : ""}
+          </div>
+        </div>
+      );
+    })
 
-  return (
-    <div>
-      {photoList}
-    </div>
-  )
+      return (
+        <div>
+          <BackArrow />
+          <NextArrow />
+          {photoList}
+        </div>
+      )
 
   }
 }
