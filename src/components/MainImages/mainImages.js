79ff900a064f24ee1690c9602a36f5a83d10f96d
Sam <sammintz1993@gmail.com>
2019-07-18 21:03:01 -0500

    Added Link to MainImage page

--- src/components/MainImages/mainImages.js
+++ src/components/MainImages/mainImages.js
@@ -1,11 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
 import  "./mainImages.css";
 
 const mainImages = () => (
   <div className="imageContainer">
-    <div className="images">1</div>
-    <div className="images">2</div>
-    <div className="images">3</div>
+    <div className="images">
+      <img src="" alt="picture1" />
+      <h2>
+        <Link to={"/post-two"}>Recipe</Link>
+      </h2>
+    </div>
+    <div className="images">
+      <img src="" alt="picture2" />
+      <Link />
+    </div>
+    <div className="images">
+      <img src="" alt="picture3" />
+      <Link />
+    </div>
   </div>
 )
 
