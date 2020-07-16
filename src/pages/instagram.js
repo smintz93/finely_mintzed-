9bc56fdb31a3d5f9dd0770fb38351f023bd2d407
Sam <sammintz1993@gmail.com>
2020-07-16 14:24:37 -0500

    Removed Instagram connection to test

--- src/pages/instagram.js
+++ src/pages/instagram.js
@@ -62,7 +62,7 @@ class Instagram extends Component {
   render() {
     // { console.log(this.state.photos) }
     return (
-      <div class='instaDescription' style={{marginTop: '2.45rem'}}>
+      <div class='instaDescription' style={{display: 'none', marginTop: '2.45rem'}}>
         <p>Take a look at some of my most recent creations below or head on over to the <a href='https://www.instagram.com/finelymintzed/' target="_blank" >finelymintzed</a> Instagram</p>
       <div className='insta'>  
         {this.state.slideCount !== 0 ? <BackArrow previousImage={this.previousImage}/> : ''}
