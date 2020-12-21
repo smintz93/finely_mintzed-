cdec24ff61656f334e2c97cdf43d34a1a799e9d4
Sam Mintz <sammintz@Sams-MacBook-Pro.local>
2020-12-21 16:36:57 -0600

    Updated About page

--- src/pages/2020-paleo-cookies/index.md
+++ src/pages/2020-paleo-cookies/index.md
@@ -1,45 +1,45 @@
 ---
 path: "/paleochocolatechipcookies"
 date: "June 24, 2020"
-title: "Paleo Chewy Chocolate Chip Cookies" 
+title: "Paleo Chewy Chocolate Chip Cookies"
 # image: ../../images/frittata.jpg
 author: "@finelymintzed"
 ---
+
 ![Paleo Chewy Chocolate Chip Cookies](./paleocookies.jpg)
 
+### Makes 10-12 Cookies
 
-### Makes 10-12 Cookies 
+### Recipe
 
-### Recipe 
-* 1 egg
+- 1 egg
 
-* ¼ cup coconut oil, melted and cooled
+- ¼ cup coconut oil, melted and cooled
 
-* 1 t vanilla extract
-* ¼ cup creamy almond butter (should be pretty drippy, another nut butter could also work here!)
+- 1 t vanilla extract
+- ¼ cup creamy almond butter (should be pretty drippy, another nut butter could also work here!)
 
-* ½ cup coconut sugar
-* 1 cup almond flour
-* 2 T coconut flour
-* ½ tsp baking soda
-* pinch of sea salt
-* ½ dairy free cup chocolate chips
-
-
+- ½ cup coconut sugar
+- 1 cup almond flour
+- 2 T coconut flour
+- ½ tsp baking soda
+- pinch of sea salt
+- ½ dairy free cup chocolate chips
 
 ### Instructions
-1.     Preheat oven to 350 degrees and line a large sheet pan with parchment paper
 
-2.     Beat egg and whisk in melted coconut oil, coconut sugar, vanilla extra, and almond butter until well combined
+1.      Preheat oven to 350 degrees and line a large sheet pan with parchment paper
 
-3.     Add in almond flour, coconut flour, baking soda and salt and mix well until all the flour is mixed into the dough (dough will be a little sticky)
+2.      Beat egg and whisk in melted coconut oil, coconut sugar, vanilla extract, and almond butter until well combined
 
-4.     Fold in chocolate chips
+3.      Add in almond flour, coconut flour, baking soda and salt and mix well until all the flour is mixed into the dough (dough will be a little sticky)
 
-5.     Use spoon to help scoop out cookies onto parchment paper
+4.      Fold in chocolate chips
 
-6.     Wet hands slightly with water to help shape cookies, pressing down slightly
+5.      Use spoon to help scoop out cookies onto parchment paper
 
-7.     Bake cookies for 10-12 minutes until edges start to brown slightly
+6.      Wet hands slightly with water to help shape cookies, pressing down slightly
 
-8.     Let cookies cool and enjoy! 
+7.      Bake cookies for 10-12 minutes until edges start to brown slightly
+
+8.      Let cookies cool and enjoy!
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,53 +1,52 @@
-import React from 'react';
-import Layout from "../components/layout";
-import './about.css'
-import JoBioPic from '../images/JohannaBio.jpeg';
+import React from "react"
+import Layout from "../components/layout"
+import "./about.css"
+import JoBioPic from "../images/JohannaBio.jpeg"
 
 const About = () => (
   <Layout>
-    <div className='pageAboutWrapper'>
-    <h1>About</h1>
-    <div class='aboutContainerMain'>
-    <img className="JoBio" src={JoBioPic} />
-    <div class='aboutTextMain'>
-      <p>
-          Hello everyone! My name is Johanna Mintz and I am so excited to welcome
-          you to my blog, finelymintzed. I am originally from Chicago, but currently
-          living in Washington, DC, after graduating from Georgetown University in
-        2018.
-      </p> 
-      <p>
-          While I enjoyed cooking throughout college, my love for cooking and
-          crafting new recipes in the kitchen has sky-rocketed in my post-grad
-          life.
-      </p>
-      <p>
-          My favorite way to unwind after work is to head straight to the kitchen
-        and start prepping dinner (or a post-dinner treat). My love for all thing
-        paleo started in high school when my Mom and I thought we would try out a
-        10-day paleo plan. It was not exactly love at first bite 😉, as our bodies
-        had to adjust to this new way of eating, but we decided to continue eating
-        paleo after those 10 days and have since become utterly hooked!
-      </p>
-      <p>
-          There is no better feeling than fueling your body with real, whole,
-          nutrient-dense foods. I feel the best when I eat paleo, but I also am
-          strongly against deprivation and very much support a balanced, healthy
-          way of life. So while most of the recipes you’ll see here are paleo, I
-          also incorporate some non-paleo goods, and those who know me know that I
-          will rarely say no to a good old-fashioned (non-paleo) treat.
-      </p>
-      <p>
-          I hope after taking a peek around finelymintzed you realize how fun and
-          easy it is to make simple, delicious, and healthy meals (and desserts)
-          and feel inspired to get cooking!
-      </p>
-    </div>
-    </div>
+    <div className="pageAboutWrapper">
+      <h1>About</h1>
+      <div class="aboutContainerMain">
+        <img className="JoBio" src={JoBioPic} />
+        <div class="aboutTextMain">
+          <p>
+            Hello everyone! My name is Johanna Mintz and I am so excited to
+            welcome you to my blog, finelymintzed. I am originally from Chicago,
+            but currently living in Washington, DC, attending law school.
+          </p>
+          <p>
+            While I enjoyed cooking throughout college, my love for cooking and
+            crafting new recipes in the kitchen has sky-rocketed in my
+            post-college life.
+          </p>
+          <p>
+            My favorite way to unwind is to head straight to the kitchen and
+            start prepping dinner (or a post-dinner treat). My love for all
+            thing paleo started in high school when my Mom and I thought we
+            would try out a 10-day paleo plan. It was not exactly love at first
+            bite 😉, as our bodies had to adjust to this new way of eating, but
+            we decided to continue eating paleo after those 10 days and have
+            since become utterly hooked!
+          </p>
+          <p>
+            There is no better feeling than fueling your body with real, whole,
+            nutrient-dense foods. I feel the best when I eat paleo, but I also
+            am strongly against deprivation and very much support a balanced,
+            healthy way of life. So while most of the recipes you’ll see here
+            are paleo, I also incorporate some non-paleo goods, and those who
+            know me know that I will rarely say no to a good old-fashioned
+            (non-paleo) treat.
+          </p>
+          <p>
+            I hope after taking a peek around finelymintzed you realize how fun
+            and easy it is to make simple, delicious, and healthy meals (and
+            desserts) and feel inspired to get cooking!
+          </p>
+        </div>
+      </div>
     </div>
   </Layout>
 )
-     
 
-
-export default About;
+export default About
