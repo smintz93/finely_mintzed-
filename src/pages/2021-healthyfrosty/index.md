643b6f11dc07a66e93255aefce1a9d52b8e32d38
Sam Mintz <sammintz@Sams-MacBook-Pro.local>
2021-01-28 18:00:33 -0600

    Frosty recipe added

---
+++ src/pages/2021-healthyfrosty/index.md
@@ -0,0 +1,39 @@
+---
+path: "/healthychocolatefrosty"
+date: "January 28, 2021"
+title: "Healthy Chocolate Frosty"
+# image: ../../images/bpancake.jpg
+author: "@finelymintzed"
+---
+
+![Healthy Chocolate Frosty](./frosty.jpg)
+
+### Recipe
+
+- 1 frozen banana
+
+- 1 cup frozen cauliflower rice
+
+- 1 cup ice
+
+- 1 date (optional, or drizzle of honey to sweeten)
+
+- 1 T almond butter
+
+- 1 T cacao powder
+
+- ½ t vanilla extract
+
+- ½ t cinnamon
+
+- 1 scoop collagen or protein powder
+
+- Almond milk/coconut water to blend
+
+### Instructions
+
+1.       Blend everything together in a high-powered blender
+
+2.       Start with a small amount of liquid and add more as necessary to blend (it helps to use a tamper stick to get things going)
+
+3.  Enjoy
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,32 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import MainImages from "../components/MainImages/mainImages"
-import MainAbout from "../components/MainAbout/mainAbout";
+import MainAbout from "../components/MainAbout/mainAbout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Instagram from "./instagram"
-import './about.css'
-
-
+import "./about.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     {/* <h1 class='welcome' style={{marginBottom: '45x'}}>Welcome to Finely Mintzed</h1> */}
-    <div style={{
-        // display: 'flex',
-        // flexDirection: 'column',
-        // justifyContent: 'space-around'
-        
-    }}>
+    <div
+      style={
+        {
+          // display: 'flex',
+          // flexDirection: 'column',
+          // justifyContent: 'space-around'
+        }
+      }
+    >
       <MainImages />
       <MainAbout />
-      <div style={{marginBottom: `1.45rem`}}>
+      <div style={{ marginBottom: `1.45rem` }}>
         <Instagram />
         {/* <Image /> */}
       </div>
     </div>
-  
   </Layout>
 )
 
